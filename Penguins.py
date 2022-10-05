4b853d3dd12f85bfbde9664cb0d9924a12e0b874
NolanKasperowicz-10 <831223@mystma.org>
2022-10-05 15:19:51 -0500

    asd

---
+++ Penguins.py
@@ -0,0 +1,25 @@
+penguins = ["King", "Emperor", "Chinstrap", "Gentoo", "Little", "African", "Southern Rockhopper", "Macaroni"]
+pHab = ["King penguins spend a lot of time in the ocean feeding,\n but their primary habitats are sparsely vegetated areas of islands in the southern oceans and sub-Antarctic"]
+pDiet = []
+pApp = []
+pConStat = []
+print("Hello! Welcome to Penguin Facts! What Penguin would you like to learn about?")
+p = input("0. King\n1. Emperor\n")
+print("What do you want to learn about the " + penguins[int(p)] + " penguin?")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
